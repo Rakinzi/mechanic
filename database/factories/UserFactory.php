1c4030707bb52c19b7a31f18857dbb6f686dcc36
Rakinzi Silver <silverrakinzi@gmail.com>
2026-06-17 10:25:32 +0200

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,9 +23,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        static $counter = 0;
+        $counter++;
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'User '.$counter,
+            'email' => 'user'.$counter.'@example.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
